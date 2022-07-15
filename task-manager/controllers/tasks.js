5de5ab506dfe501a669ae58dc14731b3d2ebbcc3
DebasmitBiswal <debasmitbiswal@outlook.com>
2022-07-15 16:50:07 +0530

    Added restapi service

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -1,7 +1,27 @@
 const getAllTasks = (req, res) => {
-  res.send(`all items`);
+  res.send(`all items from the file`);
+};
+
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  res.send(`Update Task`);
+};
+
+const deleteTask = (req, res) => {
+  res.send(`Delete task`);
 };
 
 module.exports = {
   getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 };
